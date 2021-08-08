53c90bae3e2e4f8b3faf8649e7cf887908f9c4dc
sunqingzhe802 <bye_blue@sina.cn>
2021-08-08 21:18:40 +0800

    0807_23:59

--- Day01/src/cn/tedu/hello/Num99.java
+++ Day01/src/cn/tedu/hello/Num99.java
@@ -2,6 +2,11 @@ package cn.tedu.hello;
 
 public class Num99 {
     public static void main(String[] args) {
-
+        for(int i =1;i<=9;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+"*"+i+"="+j*i+"\t");
+            }
+            System.out.println();
+        }
     }
 }
